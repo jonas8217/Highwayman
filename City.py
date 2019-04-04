ead86c66a046f16c04ab574d474d20df2c83b294
jonas8217 <Jonas82173000@gmail.com>
2019-04-04 13:46:27 +0200

    Update City.py

--- City.py
+++ City.py
@@ -6,3 +6,4 @@ class City:
         self.size = randint(11,15)
         start_col = randint(100,215)
         self.col = (start_col + randint(0,80) - 40, start_col + randint(0,80) - 40, start_col + randint(0,80) - 40)
+        self.resources = [randint(0,2),randint(0,2),randint(0,2)]
