419a7cf983fea65dfb4ced4b6b29268d98560ae9
jonas8217 <Jonas82173000@gmail.com>
2019-04-19 13:11:29 +0200

    Legacy code is done for finally fucking fixed

--- City.py
+++ City.py
@@ -6,7 +6,8 @@ class City:
         self.size = randint(11,15)
         start_col = randint(100,215)
         self.col = (start_col + randint(0,80) - 40, start_col + randint(0,80) - 40, start_col + randint(0,80) - 40)
-        self.resources = [randint(0,2),randint(0,2),randint(0,2)]
+        
 
-    def __repr__ (self):
+
+    def __repr__ (self): #for debugging purposes
         return 'City: pos(' + str(self.pos[0]) + ',' + str(self.pos[1]) + ')'
